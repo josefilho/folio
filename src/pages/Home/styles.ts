7d3e1f8976b5687c7a8ed3eacdd0ff925e667e54
Jose C. S. Filho <iamjose.filho@gmail.com>
2022-06-20 19:44:53 -0400

    feat(home): Added responsiveness
    Refactor code to improve the understanding

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,12 +1,10 @@
 import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 4%;
-`;
+/*
+  !------------------------------------------------------------------------
+  ! @keyframes
+  !------------------------------------------------------------------------
+*/
 
 const appearFromLeft = keyframes`
   from {
@@ -16,42 +14,6 @@ const appearFromLeft = keyframes`
   to {
     opacity: 1;
     transform: translateX(0);
-  }
-`;
-
-export const Text = styled.h1`
-  flex: 1;
-  font-size: 18px;
-  font-weight: normal;
-  text-align: justify;
-  color: #ede5ff;
-  width: 50%;
-  padding: 5vh 8vw;
-
-  animation: ${appearFromLeft} 1s;
-
-  @media (min-width: 885px) {
-    width: 70%;
-    font-size: 24px;
-  }
-
-  @media (min-height: 610px) {
-    font-size: 16px;
-  }
-`;
-
-export const TextHighlight = styled.i`
-  font-size: 18px;
-  font-weight: bold;
-  color: #6a2cf2;
-
-  animation: ${appearFromLeft} 1s;
-  @media (max-width: 885px) {
-    font-size: 24px;
-  }
-
-  @media (min-height: 610px) {
-    font-size: 16px;
   }
 `;
 
@@ -66,13 +28,112 @@ const appearFromRight = keyframes`
   }
 `;
 
-export const Image = styled.img`
-  animation: ${appearFromRight} 1s;
-  width: 450px;
-  right: 0;
-  margin-right: 46px;
+/*
+ *------------------------------------------------------------------------
+ * Styles
+ *------------------------------------------------------------------------
+ */
 
-  @media (max-width: 885px) {
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 100px);
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextContainer = styled.div`
+  font-size: 1rem;
+  text-align: justify;
+  width: 45vw;
+
+  // * Responsiveness
+  @media (min-height: 768px) {
+    font-size: 1.25rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 90vw;
+  }
+
+  @media (max-width: 600px) {
+    height: calc(100vh - 100px);
+  }
+
+  @media (max-height: 560px) {
+    font-size: 1.1rem;
+  }
+
+  @media (max-height: 480px) {
+    font-size: 0.8rem;
+  }
+`;
+
+export const Text = styled.p`
+  color: #ede5ff;
+
+  font-size: 1em;
+
+  animation: ${appearFromLeft} 1s;
+
+  // * Responsiveness
+
+  @media (max-width: 768px) {
+    font-size: 1.3em;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 1.3em;
+    margin-top: 2vh;
+  }
+
+  @media (max-width: 560px) {
+    font-size: 1em;
+  }
+`;
+
+export const TextHighlight = styled.i`
+  font-weight: bold;
+  color: #6a2cf2;
+
+  font-size: 1.05em;
+
+  animation: ${appearFromLeft} 1s;
+
+  // * Responsiveness
+
+  @media (max-width: 768px) {
+    font-size: 1.3em;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 1em;
+  }
+
+  @media (max-width: 560px) {
+    font-size: 1em;
+  }
+`;
+
+export const Image = styled.img`
+  width: 28vw;
+
+  animation: ${appearFromRight} 1s;
+
+  margin-left: 60px;
+
+  // * Responsiveness
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  @media (max-height: 560px) {
+    width: 30vw;
+  }
+
+  @media (max-height: 480px) {
     display: none;
   }
 `;
